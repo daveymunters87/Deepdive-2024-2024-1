2a181ecc3581f57f34162a84130512b9dca3daff
davey <daveymunters87@gmail.com>
2024-09-10 13:00:10 +0200

    Add: Added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/src/**/*.{html,js}",  
+    "./src/**/*.{html,js}"         
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
